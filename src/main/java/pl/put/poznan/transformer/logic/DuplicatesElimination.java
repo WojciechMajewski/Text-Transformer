6ee24237bed78286e706bc4c6e264f26313d1f0b
senketsutsu <100175419+senketsutsu@users.noreply.github.com>
2022-05-21 17:01:32 +0200

    Update DuplicatesElimination.java
    done

--- src/main/java/pl/put/poznan/transformer/logic/DuplicatesElimination.java
+++ src/main/java/pl/put/poznan/transformer/logic/DuplicatesElimination.java
@@ -5,6 +5,27 @@ public class DuplicatesElimination extends DecoratedTextTransformer{
     public DuplicatesElimination(TextTransformer TT){
         this.text = TT.getText();
         //place for transformation
+        String[] c = (this.text).split(" ",-2);
+        String done = c[0]+" ";
+        String last = c[0];
+        String now;
+        for(int j=1; j<c.length; j++)
+        {
+            now = c[j];
+            
+            if(!(last.equals(now)))
+            {
+                done = done + now;
+                last = now;
+                if(j<c.length-1)
+                {
+                    done = done + " ";
+                }
+            }
+            
+        }
+        this.text = done;
+        
     }
     @Override
     public String getText(){
