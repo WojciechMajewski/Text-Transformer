f86aac02d2eafd7fa92236b3efb5fbf96d4d2457
adam-cie <87044575+adam-cie@users.noreply.github.com>
2022-05-21 18:39:25 +0200

    adding specialSign constructor

--- src/main/java/pl/put/poznan/transformer/logic/SpecialSings.java
+++ src/main/java/pl/put/poznan/transformer/logic/SpecialSings.java
@@ -4,7 +4,62 @@ public class SpecialSings extends Convert{
     private String text;
     public SpecialSings(TextTransformer TT){
         this.text = TT.getText();
-        //place for transformation
+        String newText = "";
+        String a = "%${_|>-#&}<";
+        char[] b = a.toCharArray();
+        for(int i=0; i<text.length(); i++)
+        {
+            if(text.charAt(i) == b[0])
+            {
+                newText += "\\%";
+            }
+            else if(text.charAt(i) == b[1])
+            {
+                newText += "\\$";
+            }
+            else if(text.charAt(i) == b[2])
+            {
+                newText += "\\{";
+            }
+            else if(text.charAt(i) == b[3])
+            {
+                newText += "\\_";
+            }
+            else if(text.charAt(i) == b[4])
+            {
+                newText += "\\textbar";
+            }
+            else if(text.charAt(i) == b[5])
+            {
+                newText += "\\textgreater";
+            }
+            else if(text.charAt(i) == b[6])
+            {
+                newText += "\\textendash";
+            }
+            else if(text.charAt(i) == b[7])
+            {
+                newText += "\\#";
+            }
+            else if(text.charAt(i) == b[8])
+            {
+                newText += "\\&";
+            }
+            else if(text.charAt(i) == b[9])
+            {
+                newText += "\\}";
+            }
+            else if(text.charAt(i) == b[10])
+            {
+                newText += "\\textless";
+            }
+            else
+            {
+                newText += text.charAt(i);
+            }
+
+        }
+        this.text = newText;
     }
     @Override
     public String getText(){
