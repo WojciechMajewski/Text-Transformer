9ab10d167184b40cf0b32c0dca2f0bcc7318eb3e
senketsutsu <100175419+senketsutsu@users.noreply.github.com>
2022-05-21 14:57:42 +0200

    Update Capitalized.java
    not sure if working, but it worked on strings, but yeah

--- src/main/java/pl/put/poznan/transformer/logic/Capitalized.java
+++ src/main/java/pl/put/poznan/transformer/logic/Capitalized.java
@@ -4,8 +4,34 @@ public class Capitalized extends SizeModifier{
     private String text;
     public Capitalized(TextTransformer TT){
         this.text = TT.getText();
-        //place for transformation
+        String[] splited = this.text.split("", -1);
+        String done = "";
+        Character letter;
+        Boolean check=true;
+        // in java <8 there is an empty element at indx 0
+        for(int i=0; i<this.text.length(); i++)
+        {
+            letter = this.text.charAt(i);
+            if( Character.isLetter(letter))
+            {
+                if(check)
+                {
+                    done = done+upper(splited[i]);
+                    check = false;
+                }
+                else{
+                    done = done+lower(splited[i]);
+                }
+            }
+            else
+            {
+                done = done+splited[i];
+                check = true;
+            }
+        }
+        this.text = done;
     }
+    
     @Override
     public String getText(){
 
