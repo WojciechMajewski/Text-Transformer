0947cf182a89814ac38c0ca4d0d65f98baa484a9
senketsutsu <100175419+senketsutsu@users.noreply.github.com>
2022-05-22 21:57:03 +0200

    add comments for javadoc

--- src/main/java/pl/put/poznan/transformer/logic/Lower.java
+++ src/main/java/pl/put/poznan/transformer/logic/Lower.java
@@ -1,7 +1,17 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * A class for transforming text to the lower case
+ */
+
 public class Lower extends SizeModifier{
     private String text;
+    
+    /**
+     * Modify the case of letters to lower case
+     * @param TT
+     */
+    
     public Lower(TextTransformer TT){
         this.text = TT.getText();
         this.text = this.text.toLowerCase();
